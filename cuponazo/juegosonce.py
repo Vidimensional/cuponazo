96d77993f6e1bfd02db2a4732bca1a3f3f54cd7c
Daniel Vidal de la Rubia <dvrubia@gmail.com>
2023-09-10 14:21:59 +0200

    Fetcher returns cuponazo result instead of dict

--- cuponazo/juegosonce.py
+++ cuponazo/juegosonce.py
@@ -5,15 +5,24 @@ lottery_names = {"cuponazo": "Cuponazo", "cupon_diario": "Cup&oacute;n Diario"}
 url = "https://www.juegosonce.es/rss/sorteos2.xml"
 
 
+class CuponazoResult:
+    def __init__(self, number: str, serie: str):
+        self.number = number
+        self.serie = serie
+
+
 class ResultsFetcher:
     def __init__(self, url: str, http: requests.Session) -> None:
         self.url = url
         self.http = http
 
-    def fetch_cuponazo(self) -> str:
-        return self.fetch("cuponazo")
+    def fetch_cuponazo(self) -> list[CuponazoResult]:
+        return [
+            CuponazoResult(item["numero"], item["serie"])
+            for item in self.__fetch("cuponazo")
+        ]
 
-    def fetch(self, lottery_type: str) -> str:
+    def __fetch(self, lottery_type: str) -> list[dict[str, str]]:
         resp = self.http.get(self.url)
 
         return [
--- test/test_juegosonce.py
+++ test/test_juegosonce.py
@@ -2,6 +2,7 @@ import json
 import unittest
 from unittest.mock import Mock
 
+from cuponazo.juegosonce import CuponazoResult
 from cuponazo.juegosonce import ResultsFetcher
 from cuponazo.juegosonce import url as juegosonce_url
 
@@ -20,7 +21,9 @@ class TestResultFetcher(unittest.TestCase):
         fetcher = ResultsFetcher(juegosonce_url, mocked_http)
         result = fetcher.fetch_cuponazo()
 
-        self.assertEqual(result, load_expected_result(raffle_type, test_case))
+        self.assertTrue(len(result), 1)
+        self.assertEqual(result[0].number, "75727")
+        self.assertEqual(result[0].serie, "024")
 
     def test_fetch_cuponazo_juegosonce_returns_response_without_cuponazo(self):
         raffle_type = "cuponazo"
@@ -36,14 +39,9 @@ class TestResultFetcher(unittest.TestCase):
         result = fetcher.fetch_cuponazo()
 
         # FIXME should we expect an exception here?
-        self.assertEqual(result, load_expected_result(raffle_type, test_case))
+        self.assertEqual(result, [])
 
 
 def load_fixture(case: str) -> str:
     with open(f"test/testdata/juegosonce_remote_{case}.xml") as f:
         return f.read()
-
-
-def load_expected_result(lottery_type: str, case: str) -> str:
-    with open(f"test/testdata/juegosonce_{lottery_type}_fetcher_{case}.json") as f:
-        return json.loads(f.read())
