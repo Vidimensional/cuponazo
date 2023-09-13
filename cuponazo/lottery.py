3d77fb2cf14363e0c4b37f1ab39defbd4a420ac2
Daniel Vidal de la Rubia <dvrubia@gmail.com>
2023-09-13 22:58:51 +0200

    TicketChecker gets the number of prize

--- cuponazo/lottery.py
+++ cuponazo/lottery.py
@@ -6,9 +6,12 @@ class InvalidTicketFormatError(Exception):
 
 
 class CuponazoTicket:
+    number_length = 5
+    serie_length = 3
+
     def __init__(self, number: str, serie: str) -> None:
-        number_regex = r"^[0-9]{5}$"
-        serie_regex = r"^[0-9]{3}$"
+        number_regex = r"^[0-9]{%s}$" % self.number_length
+        serie_regex = r"^[0-9]{%s}$" % self.serie_length
 
         if not re.match(number_regex, number):
             raise InvalidTicketFormatError(
@@ -38,5 +41,25 @@ class TicketChecker:
     def __init__(self, result: CuponazoTicket) -> None:
         self.result = result
 
-    def check_ticket(self, ticket: CuponazoTicket) -> None:
-        pass
+    def check_ticket(self, ticket: CuponazoTicket) -> int:
+        if ticket.number == self.result.number:
+            if ticket.serie == self.result.serie:
+                return CuponazoTicket.number_length + 1
+            else:
+                return CuponazoTicket.number_length
+
+        i_forward = self.__get_prize_from_number(self.result.number, ticket.number)
+        i_reverse = self.__get_prize_from_number(
+            self.result.number[::-1],
+            ticket.number[::-1],
+        )
+
+        return max(i_forward, i_reverse)
+
+    def __get_prize_from_number(self, result_number: str, ticket_number: str) -> int:
+        for i in range(CuponazoTicket.number_length):
+            result_nimber = result_number[i]
+            nimber = ticket_number[i]
+            if result_nimber != nimber:
+                break
+        return i
--- test/unit/lottery.py
+++ test/unit/lottery.py
@@ -1,6 +1,6 @@
 from unittest import TestCase
 
-from cuponazo.lottery import CuponazoTicket, InvalidTicketFormatError
+from cuponazo.lottery import CuponazoTicket, InvalidTicketFormatError, TicketChecker
 
 
 class Test_CuponazoTicket(TestCase):
@@ -42,3 +42,79 @@ class Test_CuponazoTicket(TestCase):
         self.assertTrue(
             CuponazoTicket("12345", "123") != CuponazoTicket("54321", "321")
         )
+
+
+class Test_TicketChecker(TestCase):
+    def test_check_ticket(self):
+        test_cases = [
+            {
+                "case_name": "5 numbers + serie",
+                "ticket": CuponazoTicket("12345", "321"),
+                "expected": 6,
+            },
+            {
+                "case_name": "5 numbers",
+                "ticket": CuponazoTicket("12345", "000"),
+                "expected": 5,
+            },
+            {
+                "case_name": "4 numbers",
+                "ticket": CuponazoTicket("02345", "000"),
+                "expected": 4,
+            },
+            {
+                "case_name": "3 numbers",
+                "ticket": CuponazoTicket("00345", "000"),
+                "expected": 3,
+            },
+            {
+                "case_name": "2 numbers",
+                "ticket": CuponazoTicket("00045", "000"),
+                "expected": 2,
+            },
+            {
+                "case_name": "1 numbers",
+                "ticket": CuponazoTicket("00005", "000"),
+                "expected": 1,
+            },
+            {
+                "case_name": "4 reversed numbers",
+                "ticket": CuponazoTicket("12340", "000"),
+                "expected": 4,
+            },
+            {
+                "case_name": "3 reversed numbers",
+                "ticket": CuponazoTicket("12300", "000"),
+                "expected": 3,
+            },
+            {
+                "case_name": "2 reversed numbers",
+                "ticket": CuponazoTicket("12000", "000"),
+                "expected": 2,
+            },
+            {
+                "case_name": "1 reversed numbers",
+                "ticket": CuponazoTicket("10000", "000"),
+                "expected": 1,
+            },
+            {
+                "case_name": "0 numbers",
+                "ticket": CuponazoTicket("00000", "000"),
+                "expected": 0,
+            },
+            {
+                "case_name": "in case of prize in both ways, return the bigger",
+                "ticket": CuponazoTicket("10345", "000"),
+                "expected": 3,
+            },
+        ]
+        checker = TicketChecker(result=CuponazoTicket("12345", "321"))
+
+        for case in test_cases:
+            results = checker.check_ticket(case["ticket"])
+
+            self.assertEqual(
+                results,
+                case["expected"],
+                msg=f"failed at case '{case['case_name']}'",
+            )
